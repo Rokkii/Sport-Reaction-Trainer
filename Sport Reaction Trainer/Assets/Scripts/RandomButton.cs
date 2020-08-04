baa073d08d78367d16e581bd8b5738185ac276a2
Neil Alexander <na2482r@gre.ac.uk>
2020-08-04 17:35:31 +0100

    Initial project commit

---
+++ Sport Reaction Trainer/Assets/Scripts/RandomButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomButton : MonoBehaviour
+{
+
+    [SerializeField]
+    private Button[] buttonArray;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RandomButtonPicker()
+    {
+        int indexNumber = Random.Range(0, buttonArray.Length);
+
+        Debug.Log((buttonArray[indexNumber].name));
+
+        buttonArray[indexNumber].GetComponent<Image>().color = Color.yellow;
+    }
+
+    public void ResetButtons()
+    {
+        for (int i = 0; i < buttonArray.Length; i++)
+        {
+            buttonArray[i].GetComponent<Image>().color = Color.grey;
+        }
+    }
+}
